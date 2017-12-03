6f7636d584cd42168a674d42bde4e67907d02940
01101101 <sylvere.armange@gmail.com>
2017-12-03 04:08:35 +0100

    dialog engine

---
+++ Assets/Scripts/DialogInteractableEntity.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogInteractableEntity : InteractableEntity
+{
+    
+    public Dialog[] dialogs;
+
+    int currentDialog;
+    int currentLine;
+
+    void Awake()
+    {
+        currentDialog = -1;
+        currentLine = -1;
+        NextDialog();
+    }
+
+    public void NextDialog ()
+    {
+        if (dialogs.Length <= currentDialog + 1)
+            return;
+
+        currentDialog++;
+
+        currentLine = -1;
+        NextLine();
+    }
+
+    public string NextLine ()
+    {
+        if (dialogs[currentDialog].lines.Length <= currentLine + 1)
+            return dialogs[currentDialog].lines[currentLine];
+
+        currentLine++;
+
+        return dialogs[currentDialog].lines[currentLine];
+    }
+
+    public string GetCurrentLine ()
+    {
+        return dialogs[currentDialog].lines[currentLine];
+    }
+
+}
+
+[System.Serializable]
+public class Dialog
+{
+    public string[] lines;
+}
---
+++ Assets/TestDialog.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDialog : MonoBehaviour {
+
+    public DialogInteractableEntity dial;
+
+	// Use this for initialization
+	void Start () {
+        dial = gameObject.GetComponent<DialogInteractableEntity>();
+        Debug.Log(dial.GetCurrentLine());
+        Debug.Log(dial.NextLine());
+        Debug.Log(dial.NextLine());
+
+        dial.NextDialog();
+        Debug.Log(dial.GetCurrentLine());
+        Debug.Log(dial.NextLine());
+        Debug.Log(dial.NextLine());
+        Debug.Log(dial.NextLine());
+        Debug.Log(dial.NextLine());
+        Debug.Log(dial.NextLine());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
