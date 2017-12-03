0998ae69b81f81de5b57ccdcbb80841ba0d041b1
01101101 <sylvere.armange@gmail.com>
2017-12-03 13:54:21 +0100

    glauque settings

--- Assets/Scripts/GlauqueEntity.cs
+++ Assets/Scripts/GlauqueEntity.cs
@@ -11,9 +11,19 @@ public class GlauqueEntity : MonoBehaviour
     {
 		if (steps.Length > 1)
         {
+            RunSettings settings = GameObject.Find("Scripts").GetComponent<RunSettings>();
+            int step = 1;
+
+            if (settings.glauqueLevel > 0)
+            {
+                step = settings.glauqueLevel - 1;
+                if (Random.value < settings.glauqueRatio)
+                    step = settings.glauqueLevel;
+            }
+
             SpriteRenderer sr = GetComponent<SpriteRenderer>();
             if (sr != null)
-                sr.sprite = steps[Random.Range(0, steps.Length)];
+                sr.sprite = steps[step];
             sr.flipX = Random.value > 0.5f;
             transform.localScale = new Vector3(Random.Range(0.9f, 1.1f), Random.Range(0.8f, 1.1f), 1f);
         }
