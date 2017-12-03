53ec81cce6bfa7abb79dba4948a36d2466f7ed42
matthieu <matthieu@mano.fr>
2017-12-03 11:23:41 +0100

    fill image gameOverManager

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour
 {
     public float time;
+    public Image clock;
 
     private void Update()
     {
         time -= Time.deltaTime;
+        clock.fillAmount -= 1.0f / time * Time.deltaTime;
 
-        if(time <= 0f)
+        if (clock.fillAmount <= 0f)
         {
             GameOver();
         }
