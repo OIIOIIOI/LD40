82b3a9483e91713b76ddc5726b88b68ca51a8a1f
sylvere <sylvere@mano.fr>
2017-12-03 20:42:07 +0100

    inventory color menu

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -6,24 +6,25 @@ using UnityEngine.UI;
 public class Inventory : MonoBehaviour {
     public Sprite[] inventoryImg;
     Image targetImg;
+    Player player;
 	// Use this for initialization
 	void Start () {
-        GameObject.Find("Player");
+        player=GameObject.Find("Player").GetComponent<Player>();
         targetImg = GetComponent<Image>();
 	}
 
 //Update is called once per frame
 
 void Update () {
-if (Player.hasItemScarf)
+if (player.hasItemScarf)
         {
             targetImg.sprite = inventoryImg[0];
         }
-        else if (Player.hasItemknive)
+        else if (player.hasItemKnife)
       {
             targetImg.sprite = inventoryImg[1];
         }
-        else if (Player.hasItemBattery)
+        else if (player.hasItemBattery)
       {
             targetImg.sprite = inventoryImg[2];
         }
